55eeab0c308c481f620540e4641000b16cf3879e
mrgos <mr.gosha8@gmail.com>
2026-07-03 15:01:49 +0100

    Instructor class

--- GymSystem/GymSystem/Instructor.cs
+++ GymSystem/GymSystem/Instructor.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Diagnostics;
+using Oracle.ManagedDataAccess.Client;
 
 namespace GymSystem
 {
-    internal class Instructor
+    public class Instructor
     {
+        public int InstructorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Speciality { get; set; }
+        public string Status { get; set; }
     }
 }
