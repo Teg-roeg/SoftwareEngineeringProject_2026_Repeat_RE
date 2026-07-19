51e231b9499fc95d1bff915fe9189f894eb0e88f
mrgos <mr.gosha8@gmail.com>
2026-07-19 09:50:00 +0100

    Create BOoking Load

--- GymSystem/GymSystem/FormCreateBooking.cs
+++ GymSystem/GymSystem/FormCreateBooking.cs
@@ -25,6 +25,36 @@ namespace GymSystem
             parent.Visible = true;
         }
 
+        private void FormCreateBookingLoad(object sender, EventArgs e)
+        {
+            txtBookingID.Text = Booking.GetNextBookingID().ToString("0000");
+
+            DataSet dsMembers = Member.GetActiveMembers();
+
+            cboMembers.Items.Clear();
+
+            for (int i = 0; i < dsMembers.Tables[0].Rows.Count; i++)
+            {
+                cboMembers.Items.Add(dsMembers.Tables[0].Rows[i][0] + " - " +
+                                     dsMembers.Tables[0].Rows[i][1] + " " +
+                                     dsMembers.Tables[0].Rows[i][2]);
+            }
+
+            DataSet dsClasses = GymClass.GetScheduledClasses();
+
+            cboClasses.Items.Clear();
+
+            for (int i = 0; i < dsClasses.Tables[0].Rows.Count; i++)
+            {
+                DateTime classDate = Convert.ToDateTime(dsClasses.Tables[0].Rows[i][4]);
+
+                cboClasses.Items.Add(dsClasses.Tables[0].Rows[i][0] + " - " +
+                                     dsClasses.Tables[0].Rows[i][1] + " - " +
+                                     classDate.ToShortDateString() + " - " +
+                                     dsClasses.Tables[0].Rows[i][5]);
+            }
+        }
+
         
     }
 }
